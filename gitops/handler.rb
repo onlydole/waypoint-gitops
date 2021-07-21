caf3a7f94c70b8df0831366fd14f964d7992a478
Taylor Dolezal <onlydole@gmail.com>
2021-07-21 08:53:52 -0700

    Unformalize greeting

--- gitops/handler.rb
+++ gitops/handler.rb
@@ -14,6 +14,6 @@ def handler(event:, context:)
         "headers": {
             "Content-Type": "text/html"
         },
-        "body": "<html><body><h1>Hello, #{name} from AWS Lambda?</h1></html></body>"
+        "body": "<html><body><h1>Hello there, #{name} from AWS Lambda?</h1></html></body>"
     }
 end
